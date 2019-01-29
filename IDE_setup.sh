4ba7d7d4b6cc79139f2adb32c0e81def9374a752
Łukasz Mitka <lukasz.mitka@husarion.com>
2019-01-29 10:09:54 +0100

    Added script for workspace configuration

---
+++ IDE_setup.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env sh
+
+if [ $# -eq 2 ]
+then
+    BUCKET_NAME="$1"
+    IAM_ROLE="$2"
+    echo "Bucket name is " $BUCKET_NAME
+    echo "IAM role is " $IAM_ROLE
+else
+    echo "Please run this script with two arguments"
+    echo "./IDE_setup.sh BUCKET_NAME IAM_ROLE"
+fi
+
+# update the submodules
+cd ~/environment/RoboMakerROSbotProject
+git submodule update --recursive --remote
+
+# configure project
+cd ~/environment/RoboMakerROSbotProject/
+python configure_project.py
+
+# prepare docker for armhf compilation
+cd /opt/robomaker/cross-compilation-dockerfile/
+sudo bin/build_image.bash
+
+# stop and delete all running docker containers
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
+
+# start build docker
+cd ~/environment/RoboMakerROSbotProject
+CONTAINER_ID=$(sudo docker run -v $(pwd):/ws -dt ros-cross-compile:armhf)
+echo "Cross compile started with ID: " $CONTAINER_ID
+docker exec $CONTAINER_ID  ws/armhf.sh
+
+# copy armhf bundle to S3 bucket
+cd ~/environment/RoboMakerROSbotProject
+aws s3 cp robot_ws/armhf_bundle/output.tar.gz s3://$BUCKET_NAME/RoboMakerROSbotProject/robot_ws/bundle/output.armhf.tar.gz
+
