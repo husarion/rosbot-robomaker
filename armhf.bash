f964fbd9276e9c14fb5f48552da284bec0659f70
Łukasz Mitka <lukasz.mitka@husarion.com>
2019-01-30 20:22:14 +0100

    Added package blacklist for build process

--- armhf.bash
+++ armhf.bash
@@ -4,5 +4,5 @@ cd ws/robot_ws
 apt update
 rosdep install --from-paths src --ignore-src -r -y
 colcon build --build-base armhf_build --install-base armhf_install
-colcon bundle --build-base armhf_build --install-base armhf_install --bundle-base armhf_bundle --apt-sources-list /opt/cross/apt-sources.yaml
+colcon bundle --build-base armhf_build --install-base armhf_install --bundle-base armhf_bundle --apt-sources-list /opt/cross/apt-sources.yaml --apt-package-blacklist src/blacklist
 exit
