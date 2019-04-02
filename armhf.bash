5395f43df036f8077f74417eec4b7de5ecde4b18
Łukasz Mitka <lukasz.mitka@husarion.com>
2019-04-02 10:24:38 +0200

    Add upgrade command for armhf compilation

--- armhf.bash
+++ armhf.bash
@@ -2,6 +2,7 @@
 . /opt/ros/kinetic/setup.bash
 cd ws/robot_ws
 apt update
+apt upgrade -y
 rosdep install --from-paths src --ignore-src -r -y
 colcon build --build-base armhf_build --install-base armhf_install
 colcon bundle --build-base armhf_build --install-base armhf_install --bundle-base armhf_bundle --apt-sources-list /opt/cross/apt-sources.yaml --apt-package-blacklist rosbot.blacklist
